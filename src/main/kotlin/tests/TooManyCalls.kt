440306dcc1341d3d40ed4924733ea2c68d579f3d
dnieln7 <dnieln7@gmail.com>
2023-01-04 11:29:25 -0600

    Added calls from different corroutines lab

---
+++ src/main/kotlin/tests/TooManyCalls.kt
@@ -0,0 +1,54 @@
+package tests
+
+import kotlinx.coroutines.*
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import java.time.LocalTime
+
+fun main() {
+    runBlocking {
+        val calls = TooManyCalls()
+
+        launch {
+            for (i in 1..10) {
+                val time = calls.getTime()
+
+                println("launch a loop: $i | time: $time")
+
+                delay(1000)
+            }
+        }
+
+        launch {
+            for (i in 1..10) {
+                val time = calls.getTime()
+
+                println("launch b loop: $i | time: $time")
+            }
+        }
+
+            for(i in 1..10) {
+                val time = calls.getTime()
+
+                println("loop: $i | time: $time")
+            }
+    }
+}
+
+class TooManyCalls {
+    private val mutex = Mutex()
+
+    private var getTimeLastCalledAt = 0L
+
+
+    suspend fun getTime(): String {
+        val millis = System.currentTimeMillis()
+        var date = "LOCKED"
+
+        if (millis - getTimeLastCalledAt > 3000) {
+            getTimeLastCalledAt = millis
+            date = LocalTime.now().toString()
+        }
+        return date
+    }
+}
